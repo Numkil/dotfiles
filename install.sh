af394a245d254611d5720a315ea1192d57fbfa09
Merel Jossart <merel.jossart@outlook.com>
2023-10-30 20:50:44 +0100

    restructuring

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 if [[ "${OSTYPE}" == 'darwin'* ]]; then
-  cp -f ./profile.sh ~/.bash_profile
+  cp -f ./custom/profile.sh ~/.bash_profile
 else
-  cp -f ./profile.sh ~/.bashrc
+  cp -f ./custom/profile.sh ~/.bashrc
 fi
-cp -f ./git-completion.sh ~/.git-completion.sh
-cp -f ./ssh-completion.sh ~/.ssh-completion.sh
-cp -f ./helper-functions.sh ~/.helper-functions.sh
-cp -f ./aliases.sh ~/.aliases.sh
-cp -f ./sudo.sh ~/.sudo.sh
-cp -f ./starship.toml ~/.config/starship.toml
+cp -f ./completions/git-completion.sh ~/.git-completion.sh
+cp -f ./completions/ssh-completion.sh ~/.ssh-completion.sh
+cp -f ./custom/helper-functions.sh ~/.helper-functions.sh
+cp -f ./custom/aliases.sh ~/.aliases.sh
+cp -f ./custom/sudo.sh ~/.sudo.sh
+cp -f ./starship/starship.toml ~/.config/starship.toml
 cp -rf ./lsd ~/.config/lsd
 
 echo "If no errors were reported, you can now run 'source ~/.bash_profile' to load the new settings."
