54c43cd9d34267c95a1c2e926473e81a9ccfee21
Merel Jossart <merel.jossart@outlook.com>
2024-05-17 08:26:38 +0200

    update config to match nvim 0.10.0

--- nvim/lua/plugins/corn.lua
+++ nvim/lua/plugins/corn.lua
@@ -6,31 +6,15 @@ return {
     -- turn off virtual text as we replace with lsp_lines
     vim.diagnostic.config {
       virtual_text = false,
-      -- TODO enable this when vim 0.10.0 is released
-      -- signs = {
-      --   text = {
-      --     [vim.diagnostic.severity.ERROR] = '✘',
-      --     [vim.diagnostic.severity.WARN] = '▲',
-      --     [vim.diagnostic.severity.HINT] = '⚑',
-      --     [vim.diagnostic.severity.INFO] = '»',
-      --   },
-      -- },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = '✘',
+          [vim.diagnostic.severity.WARN] = '▲',
+          [vim.diagnostic.severity.HINT] = '⚑',
+          [vim.diagnostic.severity.INFO] = '»',
+        },
+      },
     }
-
-    -- Use neat icons for diagnostics
-    -- TODO workaround until nvim 0.10.0 is released
-    local function sign_define(args)
-      vim.fn.sign_define(args.name, {
-        texthl = args.name,
-        text = args.text,
-        numhl = '',
-      })
-    end
-
-    sign_define { name = 'DiagnosticSignError', text = '✘' }
-    sign_define { name = 'DiagnosticSignWarn', text = '▲' }
-    sign_define { name = 'DiagnosticSignHint', text = '⚑' }
-    sign_define { name = 'DiagnosticSignInfo', text = '»' }
 
     -- Setup UI for diagnostics
     require('corn').setup {
