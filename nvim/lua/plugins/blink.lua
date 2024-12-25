edf77c1de86597573c5485b016fb971b7b5b2ba5
Merel Jossart <merel.jossart@outlook.com>
2024-12-25 10:08:50 +0100

    great config for blink!

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -9,29 +9,51 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 250,
+        treesitter_highlighting = true,
+      },
+    },
     keymap = {
-      preset = 'default',
-      ['<CR>'] = {
+      preset = 'enter',
+      ["<Tab>"] = {
         function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
+          return cmp.select_next()
         end,
-        'snippet_forward',
-        'fallback',
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"] = {
+        function(cmp)
+          return cmp.select_prev()
+        end,
+        "snippet_backward",
+        "fallback",
       },
     },
 
     appearance = {
-      use_nvim_cmp_as_default = true,
       nerd_font_variant = 'mono',
+    },
+
+    signature = {
+      enabled = true,
+      window = { border = "rounded" },
     },
 
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
-      min_keyword_length = 3,
+      providers = {
+        cmdline = {
+          min_keyword_length = 3,
+        },
+        buffer = {
+          min_keyword_length = 3,
+          max_items = 5,
+        }
+      }
     },
   },
   opts_extend = { 'sources.default' },
