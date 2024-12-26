c1984f7abce6f53af3f9463e66956eee622be780
Merel Jossart <merel.jossart@outlook.com>
2024-12-26 09:16:31 +0100

    Better config oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,7 +1,12 @@
 return {
   'stevearc/oil.nvim',
   config = function()
-    require('oil').setup()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,      -- Shows all hidden files
+      },
+      default_file_explorer = true, -- Ensures oil takes over directory buffers
+    })
 
     require('utils').keymapSet('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
   end,
