22bc1f586fec8641f912e12a647bbab97d4eba07
Merel Jossart <merel.jossart@outlook.com>
2024-12-26 11:42:01 +0100

    Add some window options to fzflua

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -8,20 +8,37 @@ return {
   },
   config = function()
     -- calling `setup` is optional for customization
-    require('fzf-lua').setup {}
+    require('fzf-lua').setup {
+      opts = {
+        fzf_opts = { ['--cycle'] = true, ['--layout'] = 'default', ['--marker'] = '+' },
+        winopts = {
+          width = 0.8,
+          height = 0.9,
+          preview = {
+            hidden = 'nohidden',
+            vertical = 'up:45%',
+            horizontal = 'right:50%',
+            layout = 'flex',
+            flip_columns = 120,
+            delay = 10,
+            winopts = { number = false },
+          }
+        }
+      }
+    }
 
     local builtin = require 'fzf-lua'
     require('utils').keymapSetList {
-      { 'n', '<leader><space>', builtin.buffers, { desc = 'Search in buffers' } },
-      { 'n', '<leader>/', builtin.grep_curbuf, { desc = 'Search fuzzily in current buffer' } },
-      { 'n', '<leader>sf', builtin.files, { desc = 'Search in files' } },
-      { 'n', '<leader>?', builtin.oldfiles, { desc = 'Search in oldfiles' } },
-      { 'n', '<leader>sg', builtin.live_grep, { desc = 'Search by string in cwd' } },
-      { 'n', '<leader>sw', builtin.grep_cword, { desc = 'Search current word in cwd' } },
-      { 'n', '<leader>sh', builtin.helptags, { desc = 'Search in help pages' } },
-      { 'n', '<leader>sd', builtin.diagnostics_document, { desc = 'Search in diagnostics' } },
-      { 'n', '<leader>gf', builtin.git_status, { desc = 'Show git status' } },
-      { 'n', '<leader>sr', builtin.resume, { desc = 'Resume last search' } },
+      { 'n', '<leader><space>', builtin.buffers,              { desc = 'Search in buffers' } },
+      { 'n', '<leader>/',       builtin.grep_curbuf,          { desc = 'Search fuzzily in current buffer' } },
+      { 'n', '<leader>sf',      builtin.files,                { desc = 'Search in files' } },
+      { 'n', '<leader>?',       builtin.oldfiles,             { desc = 'Search in oldfiles' } },
+      { 'n', '<leader>sg',      builtin.live_grep,            { desc = 'Search by string in cwd' } },
+      { 'n', '<leader>sw',      builtin.grep_cword,           { desc = 'Search current word in cwd' } },
+      { 'n', '<leader>sh',      builtin.helptags,             { desc = 'Search in help pages' } },
+      { 'n', '<leader>sd',      builtin.diagnostics_document, { desc = 'Search in diagnostics' } },
+      { 'n', '<leader>gf',      builtin.git_status,           { desc = 'Show git status' } },
+      { 'n', '<leader>sr',      builtin.resume,               { desc = 'Resume last search' } },
     }
   end,
 }
