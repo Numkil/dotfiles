052698a85e56b1f2a89d696f44e966d1815e1cd5
Merel Jossart <merel.jossart@outlook.com>
2025-01-16 08:51:24 +0100

    Fix lualine not showing recording state

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,12 +1,29 @@
 return {
-  "nvim-lualine/lualine.nvim",
+  'nvim-lualine/lualine.nvim',
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
     require('lualine').setup {
       options = { theme = 'everforest' },
-      extensions = { 'fzf', 'lazy', 'mason', 'man', 'oil' }
+      sections = {
+        -- Add the macro recording status in the mode section
+        lualine_b = {
+          {
+            'macro',
+            fmt = function()
+              local reg = vim.fn.reg_recording()
+              if reg ~= '' then
+                return 'Recording @' .. reg
+              end
+              return nil
+            end,
+            color = { fg = '#f57d26' },
+            draw_empty = false,
+          },
+        },
+      },
+      extensions = { 'fzf', 'lazy', 'mason', 'man', 'oil' },
     }
   end,
 }
