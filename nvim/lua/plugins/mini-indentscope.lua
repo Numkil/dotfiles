8c17b57099f67780d08b0ceec38c5049a4203312
Merel Jossart <merel.jossart@outlook.com>
2025-01-26 17:01:58 +0100

    Replace a few plugins with snacks

--- nvim/lua/plugins/mini-indentscope.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'echasnovski/mini.indentscope',
-  event = { 'BufReadPre', 'BufNewFile' },
-  config = function()
-    require('mini.indentscope').setup()
-  end,
-}
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,34 @@
+return {
+  'folke/snacks.nvim',
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      sections = {
+        { section = 'header' },
+        { section = 'keys', gap = 1, padding = 1 },
+        { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 2 },
+        { pane = 2, icon = ' ', title = 'Recent Files in current directory', cwd = true, section = 'recent_files', indent = 2, padding = 3 },
+        {
+          pane = 2,
+          icon = ' ',
+          title = 'Git Status',
+          section = 'terminal',
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = 'git status --short --branch --renames',
+          height = 10,
+          padding = 5,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = 'startup' },
+      },
+    },
+    bigfile = {},
+    indent = {},
+    input = {},
+    scope = {},
+    words = {},
+  },
+}
