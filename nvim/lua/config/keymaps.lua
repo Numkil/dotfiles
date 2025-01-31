fa816647a0a0b5e0e7353d11e37492ffb0088256
Merel Jossart <merel.jossart@outlook.com>
2025-01-31 10:32:07 +0100

    Fix W to save

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,29 +1,29 @@
 -- [[ Basic Keymaps ]]
 -- Alias :W with :w because I keep accidentally typing it
-vim.cmd [[ cnoreabbrev <expr> W getcmdtype() == ":" && getcmdline() == "W" ? "w" : "W" ]]
+vim.api.nvim_create_user_command('W', 'w', {})
 
 require('utils').keymapSetList {
   -- Because mac touch bar...
-  { '',  '<f1>',      '<Nop>' },
+  { '', '<f1>', '<Nop>' },
   -- Remap for dealing with word wrap
-  { 'n', 'k',         "v:count == 0 ? 'gk' : 'k'", { desc = 'up', expr = true } },
-  { 'n', 'j',         "v:count == 0 ? 'gj' : 'j'", { desc = 'down', expr = true } },
+  { 'n', 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'up', expr = true } },
+  { 'n', 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'down', expr = true } },
   -- More comfortably flip through buffers
-  { '',  '<leader>n', ':bnext<CR>',                { desc = 'Next buffer' } },
-  { '',  '<leader>N', ':bprevious<CR>',            { desc = 'Previous buffer' } },
+  { '', '<leader>n', ':bnext<CR>', { desc = 'Next buffer' } },
+  { '', '<leader>N', ':bprevious<CR>', { desc = 'Previous buffer' } },
   -- Easier navigation in splits
-  { 'n', '<C-k>',     ':wincmd k<CR>',             { desc = 'navigate between split' } },
-  { 'n', '<C-j>',     ':wincmd j<CR>',             { desc = 'navigate between split' } },
-  { 'n', '<C-h>',     ':wincmd h<CR>',             { desc = 'navigate between split' } },
-  { 'n', '<C-l>',     ':wincmd l<CR>',             { desc = 'navigate between split' } },
+  { 'n', '<C-k>', ':wincmd k<CR>', { desc = 'navigate between split' } },
+  { 'n', '<C-j>', ':wincmd j<CR>', { desc = 'navigate between split' } },
+  { 'n', '<C-h>', ':wincmd h<CR>', { desc = 'navigate between split' } },
+  { 'n', '<C-l>', ':wincmd l<CR>', { desc = 'navigate between split' } },
   -- scrolling remaps
-  { 'n', '<C-u>',     '<C-u>zz',                   { desc = 'Scroll up' } },
-  { 'n', '<C-d>',     '<C-d>zz',                   { desc = 'Scroll down' } },
+  { 'n', '<C-u>', '<C-u>zz', { desc = 'Scroll up' } },
+  { 'n', '<C-d>', '<C-d>zz', { desc = 'Scroll down' } },
   -- Visual shifting ( does not exit Visual mode )
-  { 'v', '<',         '<gv',                       { desc = 'Shift left without exiting V' } },
-  { 'v', '>',         '>gv',                       { desc = 'Shift rightt without exiting V' } },
+  { 'v', '<', '<gv', { desc = 'Shift left without exiting V' } },
+  { 'v', '>', '>gv', { desc = 'Shift rightt without exiting V' } },
   -- Diagnostic keymaps
-  { 'n', '<leader>q', vim.diagnostic.setloclist,   { desc = 'Open diagnostics list' } },
+  { 'n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' } },
   -- Clear notifications and search
   {
     'n',
