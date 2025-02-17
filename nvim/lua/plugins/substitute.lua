b056d3dbeb4736e21ad9a4828127e67a0bfd6d14
Merel Jossart <merel.jossart@outlook.com>
2025-02-17 10:48:04 +0100

    Fix error in substitute

--- nvim/lua/plugins/substitute.lua
+++ nvim/lua/plugins/substitute.lua
@@ -2,6 +2,8 @@ return {
   'gbprod/substitute.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
+    require('substitute').setup()
+
     vim.keymap.set('n', 's', require('substitute').operator, { noremap = true })
     vim.keymap.set('n', 'ss', require('substitute').line, { noremap = true })
     vim.keymap.set('n', 'S', require('substitute').eol, { noremap = true })
