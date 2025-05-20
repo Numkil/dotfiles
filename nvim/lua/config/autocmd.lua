d97269e30bfa31cea42ff795d9cfcee6252f72f0
Merel Jossart <merel.jossart@outlook.com>
2025-05-20 15:37:12 +0200

    Show diagnostics nicely

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -81,3 +81,9 @@ vim.api.nvim_create_autocmd('ModeChanged', {
     vim.wo.relativenumber = false
   end,
 })
+
+vim.api.nvim_create_autocmd('CursorHold', {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false, source = 'if_many' })
+  end,
+})
