fb5c0e10a39bec6a9b79282bfa1e2e341d16641f
Merel Jossart <merel.jossart@outlook.com>
2025-05-20 08:25:12 +0200

    Go back to light colours

--- nvim/lua/plugins/everforest.lua
+++ nvim/lua/plugins/everforest.lua
@@ -3,11 +3,12 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.o.background = 'dark'
+    vim.o.background = 'light'
 
     ---@diagnostic disable-next-line: missing-fields
     require('everforest').setup {
       background = 'hard',
+      ui_contrast = 'high',
       dim_inactive_windows = true,
     }
 
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -19,7 +19,13 @@ local os_name = _run_command("uname")
 local is_linux = os_name ~= "Darwin"
 
 -- color scheme
-config.color_scheme = "Everforest Dark Hard (Gogh)"
+config.color_scheme = "Everforest Light Hard (Gogh)"
+
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+	saturation = 0.24,
+	brightness = 0.5,
+}
 
 -- fonts
 config.font_size = 12.5
@@ -45,25 +51,25 @@ config.window_padding = {
 	top = "0",
 	bottom = "0",
 }
+config.window_close_confirmation = "NeverPrompt"
 
 -- keybindings
 config.use_dead_keys = false
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-	{
-		key = "C",
-		mods = "ALT",
-		action = act.AdjustPaneSize({ "Up", 5 }),
-	},
-	{
-		key = "B",
-		mods = "ALT",
-		action = act.AdjustPaneSize({ "Down", 5 }),
-	},
-	{
-		key = "V",
-		mods = "ALT",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
+	{ key = "C", mods = "ALT", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "B", mods = "ALT", action = act.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "V", mods = "ALT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "w", mods = "LEADER", action = act.ShowTabNavigator },
+	{ key = ":", mods = "LEADER", action = act.ActivateCommandPalette },
+	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "%", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
 }
 
 -- and finally, return the configuration to wezterm
